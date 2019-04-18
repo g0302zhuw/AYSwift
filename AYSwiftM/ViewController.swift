70ade5f11bdd54a35c02593018f003cc1c37fe8e
wei.zhu <zhuw@sijibao.com>
2019-04-18 18:03:26 +0800

    init project

---
+++ AYSwiftM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AYSwiftM
+//
+//  Created by zw on 2019/4/18.
+//  Copyright © 2019 zw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
